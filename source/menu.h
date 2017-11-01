7e3250b0b68479bd3a6b09f95544c6cc3e17e7c8
vano19 <vanja-chiklikchi19@ro.ru>
2017-11-01 07:33:03 +0200

    Update menu.h

--- source/menu.h
+++ source/menu.h
@@ -12,9 +12,16 @@ public:
     void howToPlay();
     void indicators(const int times, const int speed,const double distances);
     void gameOvere();
+    void outLevel();
+    void outPause();
+    void expectation();
     Positions getObjectPositions() const;
+    Menu();
+    Menu& operator =(const Menu& menu) = delete;
+    Menu(const Menu& menu) = delete;
 private:
-    Positions positions;
+    unique_ptr<Positions> positions_;
 };
 }
 #endif // MENU_H
+
