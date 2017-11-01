22c91986b4f753bd0ed8a9252fa79e55f544cb72
vano19 <vanja-chiklikchi19@ro.ru>
2017-11-01 07:30:36 +0200

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,38 +1,12 @@
 #include "matrix.h"
+#include "play.h"
 
 int main(int argc, char *argv[])
 {
-    race::Menu menu;
-    menu.howToPlay();
-    race::Control control;
-    thread Thread(&race::Control::controKeyboardl, &control);
-    race::Matrix matrix;
-    bool isDone = true;
-    while(isDone)
-    {
-        menu.indicators(control.getTime(),control.getObjectCar().getSpeed(),control.getObjectRoad().getDistances());
-        matrix.reset();
-        matrix.drawCoord(control.getObjectCar().getPosition(),control.getObjectRoad().getPoint());
-        matrix.drawCoord(control.getObjectCar().getMyPosition(),race::HEIGHT-race::LEN_CAR);
-        matrix.setBorder();
-        control.createNewCar();
-        control.limitSpeed();
-        control.swapPoint();
-        if(control.getObjectCar().hasCollition())
-        {
-            menu.gameOvere();
-            isDone = false;
-            control.activateKeyboard(false);
-        }
-        while(control.hasPause())
-        {
-            if (!control.endPause())
-                isDone = false;
-        }
-        
-        if(!control.isEndControl())
-            isDone = false;
-    }
+    race::Play play;
+    thread Thread(&race::Play::controKeyboardl, &play);
+    play.start();
     Thread.join();
     return 0;
 }
+
