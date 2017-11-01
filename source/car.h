b764a46f2d46e4a8e2af4e0c072940934e4d0818
vano19 <vanja-chiklikchi19@ro.ru>
2017-11-01 07:27:24 +0200

    Update car.h

--- source/car.h
+++ source/car.h
@@ -1,12 +1,7 @@
 #ifndef CAR_H
 #define CAR_H
 
-
-
-
-
 #include "road.h"
-
 
 namespace race
 {
@@ -16,20 +11,25 @@ class Car
 public:
     Car();
     ~Car() = default;
-    void    setMyPosition(const int position);
-    void    setPosition(const int position);
-    void    setSpeed(const int speed );
-    void    setPoint(const int point);
-    int     getSpeed() const;
-    int     getMyPosition() const;
-    int     getPosition() const;
-    bool    hasCollition() const;
+    void       setMyPosition(const int position);
+    void       setPosition(const int position);
+    void       setSpeed(const int speed );
+    void       setPoint(const int point);
+    int        getSpeed() const;
+    inline int getMyPosition() const;
+    inline int getPosition() const;
+    bool       hasCollition() const;
+    void operator ++();
+    void operator --();
+    Car& operator =(const Car& car) = delete;
+    Car(const Car& car) = delete;
+    Car(Car &&car) = default;
 private:
     int     position_;
     int     myPosition_;
     int     speed_;
     int     point_;
-
+    
 };
 }
 
